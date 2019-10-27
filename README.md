1d731b76a0377986b0a28b0cf9fd83b6baa29bb4
qing <qingcc0503@163.com>
2019-10-27 11:35:05 +0800

    test update .gitignore

--- README.md
+++
@@ -1,5 +0,0 @@
-# goblog
-
-[![Build Status](https://travis-ci.com/qingcc/go_learn.svg?branch=master)](https://travis-ci.com/qingcc/go_learn)
-
-a blog write by go
--- model/test.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Test struct {
-	Id     int64   `xorm:"pk autoincr BIGINT"  json:"id"`
-	CoinId int64   `xorm:"not null int" json:"coin_id"` //货币id
-	High   float64 `xorm:"not null decimal(12, 2)"`     //最高价
-	Low    float64 `xorm:"not null Numeric"`            //最低价
-}
